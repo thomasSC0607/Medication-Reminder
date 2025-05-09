c34b91ab1f02ff64e029307d33cdba4bbc81a407
Thomas <tbuitragou@eafit.edu.co>
2025-05-09 09:33:36 -0500

    configuraciones de Deployment

--- medical_app/medical_app/settings.py
+++ medical_app/medical_app/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+from decouple import config
+import os
+import dj_database_url
+
+# Cargar variables de entorno
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)o6$t&v7pu#dk#gbp*7m5fr22ze23#7kp-*=!d0!aqyffvbk%5"
+# Secret Key
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,10 +91,14 @@ WSGI_APPLICATION = "medical_app.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / "db.sqlite3",
+    # }
+    'default': dj_database_url.parse(
+        # Tries DATABASE_URL first (Render), falls back to EXTERNAL_DATABASE_URL (local)
+        os.environ.get('DATABASE_URL') or config('EXTERNAL_DATABASE_URL')
+    )
 }
 
 
@@ -125,7 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
